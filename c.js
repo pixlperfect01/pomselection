2a8d8938c242457c2bf747f2235a27f39fd76f77
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-05 13:53:07 -0600

    Update c.js

--- c.js
+++ c.js
@@ -11,13 +11,14 @@ function Circle(x, y) {
   }
 
   this.show = function() {
-    stroke("red");
-    strokeWeight(2);
-    noFill();
-    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    ctx.strokeStyle="blue";
+    ctx.lineWidth=2;
+    ctx.beginPath();
+    arc(this.x, this.y, this.r * 2, 0, 2 * Math.PI);
+    ctx.stroke();
   }
 
   this.edges = function() {
-    return (this.x + this.r >= width || this.x - this.r <= 0 || this.y + this.r >= height || this.y - this.r <= 0||(this.r*2)>10)
+    return (this.x + this.r >= c.width || this.x - this.r <= 0 || this.y + this.r >= c.height || this.y - this.r <= 0||(this.r*2)>10)
   }
 }
