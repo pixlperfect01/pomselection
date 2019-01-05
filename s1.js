06c44741bb817deff797c75b77c318fc00168126
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-05 14:12:09 -0600

    Update s1.js

--- s1.js
+++ s1.js
@@ -1,12 +1,16 @@
 var font;
 var vehicles = [];
+var circles;
+var spots;
+var img;
 
 function preload() {
   font = loadFont('AvenirNextLTPro-Demi.otf');
+  img = loadImage("arakelian.PNG");
 }
 
 function setup() {
-  createCanvas(713, 300);
+  createCanvas(713, 453);
   background(51);
   // textFont(font);
   // textSize(192);
@@ -26,6 +30,22 @@ function setup() {
     // strokeWeight(8);
     // point(pt.x, pt.y);
   }
+  
+  var density = displayDensity();
+  pixelDensity(1);
+  img.loadPixels();
+  spots = [];
+  circles = [];
+  for (var x = 0; x < img.width; x++) {
+    for (var y = 300; y < img.height; y++) {
+      var index = x + y * img.width;
+      var c = img.pixels[index*4];
+      var b = brightness([c]);
+      if (b > 1) {
+        spots.push(createVector(x, y));
+      }
+    }
+  }
 }
 
 function draw() {
@@ -35,5 +55,93 @@ function draw() {
     v.behaviors();
     v.update();
     v.show();
+  }
+  var total = 10;
+  var count = 0;
+  var attempts = 0;
+
+  while (count < total) {
+    var newC = newCircle();
+    if (newC !== null) {
+      circles.push(newC);
+      count++;
+    }
+    attempts++;
+    if (attempts > 1000) {
+      noLoop();
+      console.log("finished");
+      break;
+    }
+  }
+
+  for (var i = 0; i < circles.length; i++) {
+    var circle = circles[i];
+
+    if (circle.growing) {
+      if (circle.edges()) {
+        circle.growing = false;
+      } else {
+        for (var j = 0; j < circles.length; j++) {
+          var other = circles[j];
+          if (circle !== other) {
+            var d = dist(circle.x, circle.y, other.x, other.y);
+            var distance = circle.r + other.r;
+
+            if (d - 5 < distance) {
+              circle.growing = false;
+              break;
+            }
+          }
+        }
+      }
+    }
+
+    circle.show();
+    circle.grow();
+  }
+}
+
+function newCircle() {
+  var r = int(random(0, spots.length));
+  var spot = spots[r];
+  var x = spot.x;
+  var y = spot.y;
+
+  var valid = true;
+  for (var i = 0; i < circles.length; i++) {
+    var circle = circles[i];
+    var d = dist(x, y, circle.x, circle.y);
+    if (d < circle.r) {
+      valid = false;
+      break;
+    }
+  }
+  if (valid) {
+    return new Circle(x, y);
+  } else {
+    return null;
+  }
+}
+
+function Circle(x, y) {
+  this.x = x;
+  this.y = y;
+  this.r = 1;
+  this.growing = true;
+
+  this.grow = function() {
+    if (this.growing) {
+      this.r += 0.5;
+    }
+  }
+
+  this.show = function() {
+    stroke("blue");
+    strokeWidth(2);
+    ellipse(this.x, this.y, this.r * 2);
+  }
+
+  this.edges = function() {
+    return (this.x + this.r >= width || this.x - this.r <= 0 || this.y + this.r >= height || this.y - this.r <= 0|| (this.r * 2 ) > 10)
   }
 }
