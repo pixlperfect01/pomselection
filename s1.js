5f12df4235802292858e0d3509edb6e88940cfa7
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-09 09:38:31 -0600

    Update s1.js

--- s1.js
+++ s1.js
@@ -1,9 +1,17 @@
 var font;
+/*
+Wait...
+That wasn't
+supposed
+to happen
+*/
 var vehicles = [];
+var messages = [[],[],[],[],[]];
 var circles;
 var spots;
 var img;
-
+var mode = 0;
+var timer = 0;
 function preload() {
   font = loadFont('AvenirNextLTPro-Demi.otf');
   img = loadImage("arakelian.PNG");
@@ -24,20 +32,85 @@ function setup() {
 
   for (var i = 0; i < points.length; i++) {
     var pt = points[i];
-    var vehicle = new Vehicle(pt.x, pt.y);
+    var vehicle = new Vehicle(pt.x, pt.y, true);
     vehicles.push(vehicle);
-    // stroke(255);
-    // strokeWeight(8);
-    // point(pt.x, pt.y);
   }
+  points = font.textToPoints('Wait...', 25, 305, 250, {
+    sampleFactor: 0.25
+  });
+
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x, pt.y, true);
+    messages[0].push(vehicle);
+  }
+  points = font.textToPoints('That', 25, 305, 250, {
+    sampleFactor: 0.25
+  });
+
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x, pt.y, true);
+    messages[1].push(vehicle);
+  }
+  points = font.textToPoints('Wasn\'t', 25, 305, 250, {
+    sampleFactor: 0.25
+  });
+
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x, pt.y, true);
+    messages[2].push(vehicle);
+  }
+  points = font.textToPoints('Supposed', 25, 305, 250, {
+    sampleFactor: 0.25
+  });
+
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x, pt.y, true);
+    messages[3].push(vehicle);
+  }
+  points = font.textToPoints('To happen', 25, 305, 250, {
+    sampleFactor: 0.25
+  });
+
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x, pt.y, true);
+    messages[4].push(vehicle);
+  }
+}
+
+function mousePressed(){
+  if(mode===0)
+    mode=1;
 }
 
 function draw() {
   background(51);
-  for (var i = 0; i < vehicles.length; i++) {
-    var v = vehicles[i];
-    v.behaviors();
-    v.update();
-    v.show();
+   if(mode===0){
+    for (var i = 0; i < vehicles.length; i++) {
+      var v = vehicles[i];
+      v.behaviors();
+      v.update();
+      v.show();
+    }
+  }else{
+     draw2();
   }
 }
+
+function draw2(){
+  if(mode<messages.length){
+    for(var i=0;i<messages[mode].length;i++){
+      messages[mode][i].behaviors();
+      messages[mode][i].update();
+      messages[mode][i].show();
+    }
+  }
+  timer++;
+  if(timer % 200 === 0)
+    mode++;
+}
+      
